3b8a7f596d81bb3c0939b451e44c0c3433d9de6a
bjgoddard <bjgoddard94@gmail.com>
2020-01-22 09:35:52 -0800

    not even close babay

---
+++ src/Buttons.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Buttons = props => {
+    return (
+        <div className = 'Buttons'>
+    <form>
+        
+        {/* <Col sm="8"> */}
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+        {/* </Col> */}
+    </form>
+</div>
+    )
+}
+
+export default Buttons
---
+++ src/Total.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import AdvCalculator from './AdvCalculator'
+
+const Total = props => {
+
+    let [newResult, setResult] = useState('')
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Total
