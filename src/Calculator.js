3642e1342269c9efc827251710bf68584bcb6337
CCLancaster <connieclancaster@gmail.com>
2020-03-10 08:56:42 -0700

    fleshed out two more functions, still need to finish equate function to get and display answer

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,64 +7,54 @@ const Calculator = props => {
         display: "",
         operator: ""
     })
-    let selectNum = (e, newNum) => {
+
+    let selectNum = (e) => {
         e.preventDefault();
+        let newNum = e.target.innerText
         console.log(newNum)
         // if diplay: "" & newNum = 0 , display error 
         setNum({
-            equation: num.equation + newNum
+            display: num.display + newNum
         })
+        console.log(num.display)
     }
 
 // set display to the string of numbers selected
 // on click of operator
+    let selectOp = (e) => {
+        e.preventDefault();
+        let newOp = e.target.innerText
+        console.log(newOp)
+        setNum({
+            num1: num.display,
+            operator: newOp
+        })
+        console.log(num.operator)
+        console.log(num.num1)
+    }
     // set operator to what was selected
     // add current displayed string to num1
     // set display to empty string
-    // if operator is already there display error message
+    // if operator is already there display is 0 error message
+
+
 
 // on click of equal sign
     // trigger function
         //that takes num1 {operator} display = answer
         // set display to answer
         // clear operator
+        let equate = (e) => {
+            let answer = parseInt(num.num1) + num.operator.substr() + parseInt(num.display);
+            setNum({
+                 display: answer
+            })
+        }
 
 // on click of A/C
     //set num1: null, set display: "", set operator: ""
 
-    let equate = () => {
-        let answer = parseInt(num.num1) + parseInt(num.operator) + parseInt(num.display);
-        setNum({
-             display: answer
-        })
-    }
 
-    let min = () => {
-        setNum({
-            answer: parseInt(num.num1) - parseInt(num.num2)
-        })
-    }
-
-    let mult(e) {
-        setNum({
-            answer: parseInt(num1) * parseInt(num2)
-        })
-    }
-
-    let div(e) {
-        setNum({
-            answer: parseInt(num1) / parseInt(num2)
-        })
-    }
-
-    let solveEquation = (e, equation) => {
-        e.preventDefault();
-        if (num.equation.filter("+")) {
-            sum()
-        } else if (num.equation.filter("-")) {
-
-        }
-    }
     // let sum = (e, newNum) => {
     //     e.preventDefault();
     //     console.log(newNum)
@@ -77,39 +67,41 @@ const Calculator = props => {
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">{num.equation}</div>
+                <div className="answer-box">{num.display}</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top">AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => {selectOp(e);}}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" value="7" onClick={(e) => {selectNum(e, e.target.value);}}>7</button>
-                    <button className="calc-button" value="8" onClick={(e) => {selectNum(e, e.target.value);}}>8</button>
-                    <button className="calc-button" value="9" onClick={(e) => {selectNum(e, e.target.value);}}>9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" value="7" onClick={(e) => {selectNum(e);}}>7</button>
+                    <button className="calc-button" value="8" onClick={(e) => {selectNum(e);}}>8</button>
+                    <button className="calc-button" value="9" onClick={(e) => {selectNum(e);}}>9</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => {selectOp(e);}}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" value="4" onClick={(e) => {selectNum(e, e.target.value);}}>4</button>
-                    <button className="calc-button" value="5" onClick={(e) => {selectNum(e, e.target.value);}}>5</button>
-                    <button className="calc-button" value="6" onClick={(e) => {selectNum(e, e.target.value);}}>6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" value="4" onClick={(e) => {selectNum(e);}}>4</button>
+                    <button className="calc-button" value="5" onClick={(e) => {selectNum(e);}}>5</button>
+                    <button className="calc-button" value="6" onClick={(e) => {selectNum(e);}}>6</button>
+                    <button className="calc-button calc-button-op" value="-" onClick={(e) => {selectOp(e);}}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" value="1" onClick={(e) => {selectNum(e, e.target.value);}}>1</button>
-                    <button className="calc-button" value="2" onClick={(e) => {selectNum(e, e.target.value);}}>2</button>
-                    <button className="calc-button" value="3" onClick={(e) => {selectNum(e, e.target.value);}}>3</button>
-                    <button className="calc-button calc-button-op" value="+" onClick={(e) => {selectNum(e, e.target.value);}}>+</button>
+                    <button className="calc-button" value="1" onClick={(e) => {selectNum(e);}}>1</button>
+                    <button className="calc-button" value="2" onClick={(e) => {selectNum(e);}}>2</button>
+                    <button className="calc-button" value="3" onClick={(e) => {selectNum(e);}}>3</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => {selectOp(e);}}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button className="calc-button width-2" onClick={(e) => {selectNum(e);}}>0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => {equate(e);}}>=</button>
                 </div>
             </div>
         </div>
     )
 }
+
+// for operators, just take inner text (for x set it to *)
 
 export default Calculator
