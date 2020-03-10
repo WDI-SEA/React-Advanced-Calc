1015c0abfc391e40bbcf8c2a5fbae9d9b6f2dbda
deanjheikkila <deanjheikkila@gmail.com>
2020-03-09 21:07:59 -0700

    added functionality including modulus operator

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,36 +8,39 @@ const Calculator = props => {
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <p>{props.operator} {props.nextNum?props.nextNum:0}</p>
+                <div className="answer-box">{props.total}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={()=>props.clear()}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" onClick={()=>props.operate("%")}>%</button>
+                    <button className="calc-button calc-button-op" onClick={()=>props.operate("/")}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("7")}>7</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("8")}>8</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("9")}>9</button>
+                    <button className="calc-button calc-button-op" onClick={()=>props.operate("*")}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("4")}>4</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("5")}>5</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("6")}>6</button>
+                    <button className="calc-button calc-button-op" onClick={()=>props.operate("-")}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("1")}>1</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("2")}>2</button>
+                    <button className="calc-button" onClick={()=>props.tackNum("3")}>3</button>
+                    <button className="calc-button calc-button-op" onClick={()=>props.operate("+")}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button className="calc-button width-2" onClick={()=>props.tackNum("0")}>0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button calc-button-op" onClick={()=>props.evaluate()}>=</button>
+                </div>
+                <div className="message">
+                    <p>{props.message}</p>
                 </div>
             </div>
         </div>
