761fd46a4b436759439c507496bc8c10c8f9772b
Jummay77 <hansangho@yahoo.com>
2020-05-10 16:20:19 -0700

    initial commit with some work

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,9 +1,84 @@
 import React, { useState } from 'react'
 
-const Calculator = props => {
-    // Declare state variables
-
-
+    const Calculator = props => {
+        // Declare state variables
+        const [operation, setOperation] = useState('')
+        const [inputs, setInputs] = useState([])
+        const [display, setDisplay] = useState(0)
+        const [resetDisplay, setResetDisplay] = useState(false)
+    
+        const addInput = (e) => {
+            let newNum = e.target.innerText;
+    
+            //console.log(newNum);
+            if(display === 0) {
+                setDisplay(newNum)
+            } else if (resetDisplay === true){
+                setDisplay(newNum)
+                setResetDisplay(false)
+            } else {
+                let newDisplay = display + newNum
+                setDisplay(newDisplay)
+            }
+            setInputs([...inputs, newNum]) 
+        }
+        const clearInputs = (e) => {
+            setInputs([])  
+            clearDisplay()
+        }
+        const clearDisplay = () => {
+            setDisplay(0)
+        }
+        const addOperation = (e) => {
+            let operator = e.target.innerText
+            setOperation(operator)
+            let combine = ''
+            for(let i = 0; i < inputs.length; i++){
+                combine += inputs[i]
+            }
+            console.log(combine)
+            setInputs([combine])
+            setResetDisplay(true)
+    
+        }
+        const calcResult = (e) => {
+            let result = 0
+            let second = ''
+            for(let i = 1; i < inputs.length; i++) {
+                second += inputs[i];
+            }
+            console.log(second)
+    
+            console.log(inputs)
+            if(operation === '+') {
+                result = parseInt(inputs[0]) + parseInt(second)
+                setDisplay(result)
+                setInputs([])
+                setInputs([result])
+            } else if(operation === '-') {
+                result = parseInt(inputs[0]) - parseInt(second)
+                setDisplay(result)
+                setInputs([])
+                setInputs([result])
+            } else if(operation === 'x') {
+                result = parseInt(inputs[0]) * parseInt(second)
+                setDisplay(result)
+                setInputs([])
+                setInputs([result])
+            } else if(operation === '/') {
+                result = parseInt(inputs[0]) / parseInt(second)
+                setDisplay(result)
+                setInputs([])
+                setInputs([result])
+            } else if(operation === '%') {
+                result = parseInt(inputs[0]) % parseInt(second)
+                setDisplay(result)
+                setInputs([])
+                setInputs([result])
+            } else {
+                console.log("Incorrect input");
+            }
+        }
     return (
         <div className="container">
             <h1>React Calculator</h1>
