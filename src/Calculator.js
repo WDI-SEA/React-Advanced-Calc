45125459d540a0b0a8714364dbb61d63e827e435
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-05-11 09:02:06 -0700

    tried to get caclulator to work

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,73 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+let DEFAULT_VALUE = 0;
+let DEFAULT_OPERATOR = ''
+
 
 const Calculator = props => {
     // Declare state variables
+  let [number, setNumber] = useState(DEFAULT_VALUE);
+  let [operator, setOperator] = useState(DEFAULT_OPERATOR);
+  let [answer, setAnswer] = useState(DEFAULT_VALUE);
+  let [choice, setChoice] = useState(DEFAULT_VALUE);
+
+  useEffect( () => {
+          const getAnswer = async () => {
+              let calc;
+              switch (operator) {
+                case 'sub':
+                    calc = choice - number;
+                    break;
+                case 'add':
+                    calc = choice + number;
+                    break;
+                case 'mod':
+                    calc = choice % number;
+                    break;
+                  case 'div':
+                      calc = choice / number;
+                      break;
+                  case 'mul':
+                      calc = choice * number;
+                      break;
+                  default:
+                      setChoice('my Calc')
+              }
+
+
+              setAnswer(calc);
+              setChoice(calc);
+              console.log(calc);
+              console.log(answer);
+              console.log(operator);
+          }
+
+        ;
+      },);
+
+
+//clear AC
+const Clear = () => {
+  setNumber(DEFAULT_VALUE);
+  setOperator(DEFAULT_OPERATOR);
+  setChoice(DEFAULT_VALUE);
+  setAnswer(DEFAULT_VALUE);
+}
+// Const op = opperation caller
+const changeOperator = (e) => {
+  if(e.target.value === 'equal') {
+    getAnswer(answer)
+  }
+    setOperator(e.target.value);
+    }
+
+
+const changeView = (e) => {
+        let View = e.target.value;
+        setChoice(View);
+        setNumber(View);
+    }
+
 
 
     return (
@@ -9,35 +75,35 @@ const Calculator = props => {
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box" >{choice}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" onClick={Clear}>AC</button>
+                    <button className="calc-button calc-button-top" >+/-</button>
+                    <button className="calc-button calc-button-top" onClick={changeOperator} value={'mul'} >%</button>
+                    <button className="calc-button calc-button-op" onClick={changeOperator} value={'divide'} >/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={changeView} value={7} >7</button>
+                    <button className="calc-button" onClick={changeView} value={8} >8</button>
+                    <button className="calc-button" onClick={changeView} value={9} >9</button>
+                    <button className="calc-button calc-button-op" onClick={changeOperator} value={'mul'} >x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={changeView} value={4} >4</button>
+                    <button className="calc-button" onClick={changeView} value={5} >5</button>
+                    <button className="calc-button" onClick={changeView} value={6} >6</button>
+                    <button className="calc-button calc-button-op" onClick={changeOperator} value={'sub'} >-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={changeView} value={1} >1</button>
+                    <button className="calc-button" onClick={changeView} value={2} >2</button>
+                    <button className="calc-button" onClick={changeView} value={3} >3</button>
+                    <button className="calc-button calc-button-op" onClick={changeOperator} value={'add'} >+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" onClick={changeView} value={0}  >0</button>
+                    <button className="calc-button" onClick={changeView} >.</button>
+                    <button className="calc-button calc-button-op" onClick={changeOperator} value={'equal'}>=</button>
                 </div>
             </div>
         </div>
