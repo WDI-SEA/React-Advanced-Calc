ea26fc8db3e36eb5dc92939cbaaffcb8d14bbe44
karlmiche <karlymism@gmail.com>
2020-07-21 21:25:45 -0700

    forgot to initial commit, stubbed out calc logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Calculator from './Calculator'
+import Calculator from './Calculator.jsx'
 
 function App() {
+
+  // set state for the numbers to be calculated since input will be variable
+  // returns a pair of values: the current state and a function that updates it - react docs
+  let [total, setTotal] = useState(0);
+  // the operator will be a string that changes -- the button value is a string, so we start with empty string
+  let [operator, setOperator] = useState("");
+  // error message state
+  let [initErrMessage, setErrMessage] = useState("");
+  // user input consists of two numbers 
+  let [nextNumber, setNextNumber] = useState(0);
+
+  // handle when certain button operators are clicked
+
+  // probably need an onclick handler 
+
+  // clear button handler
+  const allClear = () => {
+    //set all states back to their "zeroed-out condition"
+    setTotal(0);
+    setOperator("");
+    setErrMessage("");
+    setNextNumber(0);
+  }
+
+  // grab the number
+  const handleNumberClick = (e) => {
+    let value = parseInt(e.target.innerText);
+    console.log(value);
+    setNextNumber(value);
+  }
+
+  // calc logic
+  const calculatorLogic = () => {
+    if (operator === "+") {
+      setTotal(total + nextNumber)
+    }
+    else if (operator === "-") {
+      setTotal(total - nextNumber) 
+    }
+    else if (operator === "*") {
+      setTotal(total * nextNumber)
+    }
+    else if (operator === "/") {
+      setTotal(total / nextNumber)
+    }
+    else if (operator === "%") {
+      console.log("we haven't handled this yet")
+    }
+  }
+
   return (
     <div className="App">
-      <Calculator />
+      <Calculator 
+      logic={calculatorLogic}
+      clickNumber={handleNumberClick}
+      clear={allClear}/>
     </div>
   );
 }
---
+++ src/Calculator.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Calculator = props => {
+    // Declare state variables
+
+    return (
+        <div className="container">
+            <h1>React Calculator</h1>
+            <div className="calc-container">
+                <p>Values: </p>
+                <div className="answer-box">TBD</div>
+                <div className="calc-row">
+                    <button className="calc-button calc-button-top" onClick={() => props.clear()}>AC</button>
+                    <button className="calc-button calc-button-top">+/-</button>
+                    <button className="calc-button calc-button-top" onClick={() => props.logic()}>%</button>
+                    <button className="calc-button calc-button-op" onClick={() => props.logic()}>/</button>
+                </div>
+                <div className="calc-row">
+                    <button className="calc-button" value={7} onClick={(e) => props.clickNumber(e)}>7</button>
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>8</button>
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>9</button>
+                    <button className="calc-button calc-button-op">x</button>
+                </div>
+                <div className="calc-row">
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>4</button>
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>5</button>
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>6</button>
+                    <button className="calc-button calc-button-op">-</button>
+                </div>
+                <div className="calc-row">
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>1</button>
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>2</button>
+                    <button className="calc-button" onClick={(e) => props.clickNumber(e)}>3</button>
+                    <button className="calc-button calc-button-op">+</button>
+                </div>
+                <div className="calc-row">
+                    <button className="calc-button width-2" onClick={(e) => props.clickNumber(e)}>0</button>
+                    <button className="calc-button">.</button>
+                    <button className="calc-button calc-button-op">=</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Calculator
