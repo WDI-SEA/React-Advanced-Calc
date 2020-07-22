04640060b8200d4989f3e20b7036ee358598ad04
Eliott Davidson <ulthos@gmail.com>
2020-07-21 21:04:07 -0700

    Added functionality for number buttons, clear button, operator buttons, and equals button

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react'
 
 const Calculator = props => {
-    // Declare state variables
+    const [output, setOutput] = useState("0");
+    const [storedNumber, setStoredNumber] = useState("");
+    const [storedOperation, setStoredOperation] = useState("");
+
+    const clearAll = () => {
+        setOutput("0");
+    }
+
+    const numberClick = (e) => {
+        console.log("thisNumber", e.target);
+        if (output === "0" || setStoredOperation) {
+            setOutput(e.target.innerHTML)
+        } else {
+            setOutput(output.concat(e.target.innerHTML));
+        }
+    }
+
+    const operatorClick = (e) => {
+        setStoredNumber(output);
+        setStoredOperation(e.target.innerHTML);
+    }
+
+    const equalsClick = () => {
+        switch (storedOperation) {
+            case "+":
+                setOutput(String(Number(storedNumber) + Number(output)))
+                break;
+            case "-":
+                setOutput(String(Number(storedNumber) - Number(output)))
+                break;
+            case "x":
+                setOutput(String(Number(storedNumber) * Number(output)))
+                break;
+            case "/":
+                setOutput(String(Number(storedNumber) / Number(output)))
+        }
+        setStoredOperation("");
+        setStoredNumber("");
+    }
 
 
     return (
@@ -9,35 +47,35 @@ const Calculator = props => {
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{output}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={() => clearAll()}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => operatorClick(e)}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>7</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>8</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>9</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => operatorClick(e)}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>4</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>5</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>6</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => operatorClick(e)}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>1</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>2</button>
+                    <button className="calc-button" onClick={(e) => numberClick(e)}>3</button>
+                    <button className="calc-button calc-button-op" onClick={(e) => operatorClick(e)}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button className="calc-button width-2" onClick={(e) => numberClick(e)}>0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button calc-button-op"onClick={(e) => equalsClick(e)}>=</button>
                 </div>
             </div>
         </div>
