fdbdadadd1e6bf863228331da035cb1824994e3a
Nick Hvattum <nhvattum@gmail.com>
2020-07-21 18:17:14 -0700

    can add numbers, decimal, and clear input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Calculator from './Calculator'
 
 function App() {
+  const [input, setInput] = useState([])
+
+  const addToInput = (e) => {
+    let value = e.target.innerText
+    setInput([...input, value])
+    console.log(input)
+  }
+
+  const clearInput = (e) => {
+    setInput([])
+
+  }
+
   return (
     <div className="App">
-      <Calculator />
+      <Calculator input={input} addToInput={addToInput} clear={clearInput}/>
     </div>
   );
 }
