939fd84fad4b8a83013be9ed31d688813d388861
jdephil <jennifer.dephillips@gmail.com>
2020-07-21 21:15:51 -0400

    number clicks done

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,13 +3,15 @@ import React, { useState } from 'react'
 const Calculator = props => {
     // Declare state variables
     let [clear, setClear] = useState(true)
-    let [number, setNumber] = useState([
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    ])
-    let [operator, setOperator] = useState([
-        '+', '-', '*', '/'
-    ])
-    let [display, setDisplay] = useState{[]}
+    let [number, setNumber] = useState('')
+    let [operator, setOperator] = useState('')
+    let [display, setDisplay] = useState('')
+
+    const numClick = e => {
+        if (e.target.value !== '0' || Number !== '') {
+            setNumber(Number + e.target.value)
+        }
+    }
 
 
     return (
@@ -25,21 +27,21 @@ const Calculator = props => {
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
+                    <button className="calc-button" onClick={numClick} value='7'>7</button>
+                    <button className="calc-button" onClick={numClick} value='8'>8</button>
+                    <button className="calc-button" onClick={numClick} value='9'>9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
+                    <button className="calc-button" onClick={numClick} value='4'>4</button>
+                    <button className="calc-button" onClick={numClick} value='5'>5</button>
+                    <button className="calc-button" onClick={numClick} value='6'>6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
+                    <button className="calc-button" onClick={numClick} value='1'>1</button>
+                    <button className="calc-button" onClick={numClick} value='2'>2</button>
+                    <button className="calc-button" onClick={numClick} value='3'>3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
