cfa5df7b64a88bfb212845f0a965c28d421cb307
Eliott Davidson <ulthos@gmail.com>
2020-07-21 21:28:59 -0700

    Bug fixes

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,24 +4,32 @@ const Calculator = props => {
     const [output, setOutput] = useState("0");
     const [storedNumber, setStoredNumber] = useState("");
     const [storedOperation, setStoredOperation] = useState("");
+    const [startNewNumber, setStartNewNumber] = useState(true);
+    // startNewNumber determines whether the next number click should:
+    // 1) replace the current output, or
+    // 2) concatenate to the existing output
 
     const clearAll = () => {
         setOutput("0");
+        setStartNewNumber(true);
     }
 
     const numberClick = (e) => {
-        console.log("thisNumber", e.target);
-        if (output === "0" || setStoredOperation) {
+        if (startNewNumber || (output === "0")) { 
             setOutput(e.target.innerHTML)
         } else {
             setOutput(output.concat(e.target.innerHTML));
         }
+        setStartNewNumber(false);
     }
 
     const operatorClick = (e) => {
         setStoredNumber(output);
         setStoredOperation(e.target.innerHTML);
+        setStartNewNumber(true);
     }
+    //TODO: Make operator clicks able to chain together operations if, say, the user type 9 + 3 - 5
+    // Right now, that sequence will give -2 as a result (i.e. 3 - 5)
 
     const equalsClick = () => {
         switch (storedOperation) {
@@ -39,7 +47,10 @@ const Calculator = props => {
         }
         setStoredOperation("");
         setStoredNumber("");
+        setStartNewNumber(true);
     }
+    // TODO: Make consecutive clicks of the Equals button repeat the last operation
+    // TODO: Round answers to an appropriate # of digits so that long answers don't make calc render weirdly
 
 
     return (
