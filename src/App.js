3f13d83a87473fcc2493d76923779182d07965ae
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-08-09 19:35:00 -0500

    Functional useStates and logic, testing opeartors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Calculator from './Calculator'
 
 function App() {
+  let [input, setInput] = useState([])
+  let [negative, setNegative] = useState([false])
+  let [operator, setOperator] = useState('')
+  let [num1, setNum1] = useState([])
+  let [result, setResult] = useState([])
+
+  const clearInput = (e) => {
+    setInput([])
+    setNegative([false])
+    setOperator('')
+    setNum1([])
+    setResult([])
+  }
+
+  const addToInput = (e) => {
+    let value = e.target.innerText
+    // handle decimals and push inputted numbers(first) into calc 
+    if (value !== "." && input.length <= 10) {
+      setInput([...input, value])
+    } else if (value === "." && !input.includes(".")) {
+      setInput([...input, value])
+    } else {
+      setOperator(e.target.value)
+    }
+  }
+
+
+  const isNegative = (e) => {
+    if(negative === false) {
+      setNegative(true)
+      setInput(["-", ...input])
+    } else {
+      setNegative(false)
+      setInput(input.slice(1))
+      
+    }
+  }
+
+  const mathSux = (e) => {
+    let operator = e.target.innerText;
+    let num1 = input;
+    let num2 = [];
+    let result = []
+    if(operator === "+") {
+      result = (Number(num1.toString()) + Number(num2.toString()))
+    } else if(operator === "-") {
+      result = (Number(num1.toString()) - Number(num2.toString()))
+    } else if(operator === "*") {
+      result = (Number(num1.toString()) * Number(num2.toString()))
+    } else if(operator === "%") {
+      result = (Number(num1.toString()) / Number(num2.toString()))
+    }
+    setResult(result)
+}
+
+
   return (
     <div className="App">
-      <Calculator />
+      <Calculator input={input} addToInput={addToInput} clear={clearInput} isNegative={isNegative} />
     </div>
   );
 }
--- src/Calculator.js
+++ src/Calculator.js
@@ -9,34 +9,34 @@ const Calculator = props => {
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{props.input}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
+                    <button className="calc-button calc-button-top" onClick={(e) => props.clear(e) }>AC</button>
+                    <button className="calc-button calc-button-top" onClick={(e) => props.isNegative(e) }>+/-</button>
                     <button className="calc-button calc-button-top">%</button>
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >7</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >8</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >4</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >5</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >1</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >2</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
+                    <button className="calc-button width-2" onClick={(e) => props.addToInput(e)} >0</button>
+                    <button className="calc-button" onClick={(e) => props.addToInput(e)} >.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>
             </div>
