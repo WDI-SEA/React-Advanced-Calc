612a549f137aed6b54635e6baefb89a8b12712fc
Elaine <elaine@Felipes-iMac.local>
2020-09-01 00:52:19 -0500

    trouble getting value to parseInt into a number...

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,8 +6,8 @@ const Calculator = props => {
     //for function
     let [fun, setFun] = useState()
     //for number value
-    let [num, setNum] = useState(0)
-    let [num1, setNum1] = useState(0)
+    let [num, setNum] = useState([])
+    let [num1, setNum1] = useState([])
     //on selecting a function button, make num1 = num, let num=0, let fun = value of button
     //equals button and run values
     //error handling
@@ -20,33 +20,33 @@ const Calculator = props => {
                 <p>Values: </p>
                 <div className="answer-box">{num}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top" onClick= {() => { setNum(0); setFun(''); setNum1(0)}}>AC</button>
+                    <button className="calc-button calc-button-top" onClick= {() => { setNum([]); setFun(''); setNum1([])}}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op" onClick= {() => {setFun('/'); setNum1(num); setNum(0)}}>/</button>
+                    <button className="calc-button calc-button-op" onClick= {() => {setFun('/'); setNum1(num); setNum([])}}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick= {() => { setNum(7)}}>7</button>
-                    <button className="calc-button" onClick= {() => { setNum(8)}}>8</button>
-                    <button className="calc-button" onClick= {() => { setNum(9)}} >9</button>
-                    <button className="calc-button calc-button-op" onClick={() => {setFun('*'); setNum1(num); setNum(0)}}>x</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(7))}}>7</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(8))}}>8</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(9))}} >9</button>
+                    <button className="calc-button calc-button-op" onClick={() => {setFun('*'); setNum1(num); setNum([])}}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick= {() => { setNum(4)}}>4</button>
-                    <button className="calc-button" onClick= {() => { setNum(5)}}>5</button>
-                    <button className="calc-button" onClick= {() => { setNum(6)}}>6</button>
-                    <button className="calc-button calc-button-op" onClick={() => {setFun('-'); setNum1(num); setNum(0)}}>-</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(4))}}>4</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(5))}}>5</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(6))}}>6</button>
+                    <button className="calc-button calc-button-op" onClick={() => {setFun('-'); setNum1(num); setNum([])}}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick= {() => { setNum(1)}}>1</button>
-                    <button className="calc-button" onClick= {() => { setNum(2)}}>2</button>
-                    <button className="calc-button" onClick= {() => { setNum(3)}}>3</button>
-                    <button className="calc-button calc-button-op" onClick={() => {setFun('+'); setNum1(num); setNum(0)}}>+</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(1))}}>1</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(2))}}>2</button>
+                    <button className="calc-button" onClick= {() => { setNum(num.concat(3))}}>3</button>
+                    <button className="calc-button calc-button-op" onClick={() => {setFun('+'); setNum1(num); setNum([])}}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2" onClick= {() => { setNum(0)}}>0</button>
+                    <button className="calc-button width-2" onClick= {() => { setNum(num.concat(0))}}>0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op" onClick= {() => { setNum( eval(+num1 +fun +num))}}>=</button>
+                    <button className="calc-button calc-button-op" onClick= {() => { setNum( eval((parseInt(num1)) + fun + (parseInt(num))))}}>=</button>
                 </div>
             </div>
             <h1>{fun}</h1>
