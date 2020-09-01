78b1de7c478978897bceae4d665640def9da9b15
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-31 21:17:39 -0700

    onClick={}

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,33 +17,33 @@ const Calculator = props => {
         <p>Values: </p>
         <div className="answer-box">TBD</div>
         <div className="calc-row">
-          <button className="calc-button calc-button-top">AC</button>
-          <button className="calc-button calc-button-top">+/-</button>
-          <button className="calc-button calc-button-top">%</button>
-          <button className="calc-button calc-button-op">/</button>
+          <button className="calc-button calc-button-top" onClick={}>AC</button>
+          <button className="calc-button calc-button-top" onClick={}>+/-</button>
+          <button className="calc-button calc-button-top" onClick={}>%</button>
+          <button className="calc-button calc-button-op" onClick={}>/</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button">7</button>
-          <button className="calc-button">8</button>
-          <button className="calc-button">9</button>
-          <button className="calc-button calc-button-op">x</button>
+          <button className="calc-button" onClick={}>7</button>
+          <button className="calc-button" onClick={}>8</button>
+          <button className="calc-button" onClick={}>9</button>
+          <button className="calc-button calc-button-op" onClick={}>x</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button">4</button>
-          <button className="calc-button">5</button>
-          <button className="calc-button">6</button>
-          <button className="calc-button calc-button-op">-</button>
+          <button className="calc-button" onClick={}>4</button>
+          <button className="calc-button" onClick={}>5</button>
+          <button className="calc-button" onClick={}>6</button>
+          <button className="calc-button calc-button-op" onClick={}>-</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button">1</button>
-          <button className="calc-button">2</button>
-          <button className="calc-button">3</button>
-          <button className="calc-button calc-button-op">+</button>
+          <button className="calc-button" onClick={}>1</button>
+          <button className="calc-button" onClick={}>2</button>
+          <button className="calc-button" onClick={}>3</button>
+          <button className="calc-button calc-button-op" onClick={}>+</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button width-2">0</button>
-          <button className="calc-button">.</button>
-          <button className="calc-button calc-button-op">=</button>
+          <button className="calc-button width-2" onClick={}>0</button>
+          <button className="calc-button" onClick={}>.</button>
+          <button className="calc-button calc-button-op" onClick={}>=</button>
         </div>
       </div>
     </div>
