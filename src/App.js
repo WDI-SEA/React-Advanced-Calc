6bef60e558dc91ed307b76cae792d011ce02b28b
alpha-martinez <martinezalphaa@gmail.com>
2020-09-01 00:02:59 -0400

    got it sort of working. need to take out the zero

--- src/App.js
+++ src/App.js
@@ -5,85 +5,76 @@ import Calculator from './Calculator'
 function App() {
   //useState -> the first one is its original form but the second one is what changes come next
 
-  let [num, setNum] = useState(0); //defines the num
-  let [operator, setOperator] = useState(""); //operator
-  let [total, setTotal] = useState(""); //total
-  let [error, setError] = (""); //does this work?
+  // let [num, setNum] = useState(0); //defines the num
+  // let [operator, setOperator] = useState(""); //operator
+  // let [total, setTotal] = useState(""); //total
+  // let [error, setError] = (""); //does this work?
 
-   //clear button this should be simple to do?
-   const clear = () => {
-    setTotal(0);
-    setNum("");
-    setOperator("");
-  }
-
-  //operator function for my +, -, *, /, and i guess % <- need to look up
-  const formula = () => {
-    if (operator === '+') {
-      setTotal(total + parseInt(num))
-    } else if (operator === '-') {
-      setTotal(total + parseInt(num))
-    } else if (operator === '/') {
-      setTotal(total + parseInt(num))
-    } else if (operator === '*') {
-      setTotal(total + parseInt(num))
-    } else if (operator === '%') {
-      // 100 % 7 = 2 
-      // // a = 100, n = 7
-      // 100 - (7 * floor(100/7)) = 2 | (total / num) = totalOne | (num * totalOne) = totalTwo | total - totalTwo = whatsLeft
-      let total1 = total / parseInt(num);
-      let total2 = num * Math.floor(total1);
-      let whatsLeft = total - total2;
-      setTotal(Math.round(whatsLeft));
-    }
-  }
-
-  // //error message & current number (how do i do this) does it need to show in my other functions?
-  // const err = () => {
-  //   if(!num){ //this is for my number
-  //     setError("Error: two operators can't be used")
-  //   } else {
-  //     //am i missing something else?
-  //   }
+  //  //clear button this should be simple to do?
+  //  const clear = () => {
+  //   setTotal(0);
+  //   setNum("");
+  //   setOperator("");
   // }
 
-  //current number or current state
-  // currentNum = () => {
-  //   if(num) {
-
-  //   } else {
-
+  // //operator function for my +, -, *, /, and i guess % <- need to look up
+  // const formula = () => {
+  //   if (operator === '+') {
+  //     setTotal(total + parseInt(num))
+  //   } else if (operator === '-') {
+  //     setTotal(total + parseInt(num))
+  //   } else if (operator === '/') {
+  //     setTotal(total + parseInt(num))
+  //   } else if (operator === '*') {
+  //     setTotal(total + parseInt(num))
+  //   } else if (operator === '%') {
+  //     // 100 % 7 = 2 
+  //     // // a = 100, n = 7
+  //     // 100 - (7 * floor(100/7)) = 2 | (total / num) = totalOne | (num * totalOne) = totalTwo | total - totalTwo = whatsLeft
+  //     let total1 = total / parseInt(num);
+  //     let total2 = num * Math.floor(total1);
+  //     let whatsLeft = total - total2;
+  //     setTotal(Math.round(whatsLeft));
   //   }
+  //   handleOperator();
   // }
 
-  let handleButtons = (e) => {
-    e.preventDefault();
-    console.log('please work')
-    if (num || e.target.value !== '0') {
-      setNum(num + e.target.value)
-    }
-    console.log(num)
-  }
+  // let handleButtons = (e) => {
+  //   e.preventDefault();
+  //   console.log('please work')
+  //   if (num || e.target.value !== '0') {
+  //     setNum(num + e.target.value)
+  //   }
+  //   console.log(num)
+  // }
 
-  let handleOperator = (e) => {
-    e.preventDefault();
-    console.log(e.target.value)
-    setOperator(e.target.value )
-  }
+  // let handleOperator = (e) => {
+  //   if(operator){
+  //     setError('Operator already in use')
+  //   } else if (!num) {
+  //     setError('Enter a number')
+  //   } else {
+  //     e.preventDefault();
+  //     //console.log(e.target.value)
+  //     setOperator(e.target.value)
+  //     setError('');
+  //     setNum('');
+  //   }
+  // } 
 
   return (
     <div className="App">
       <Calculator 
-      formula={formula}
-      total={total}
-      clear={clear}
-      operator={operator}
-      //err={err}
-      error={error}
-      //currentNum={currentNum}
-      handleButtons={handleButtons}
-      num={num}
-      handleOperator={handleOperator}
+      // formula={formula}
+      // total={total}
+      // clear={clear}
+      // operator={operator}
+      // //err={err}
+      // error={error}
+      // //currentNum={currentNum}
+      // handleButtons={handleButtons}
+      // num={num}
+      // handleOperator={handleOperator}
       />
     </div>
   );
--- src/Calculator.js
+++ src/Calculator.js
@@ -3,45 +3,96 @@ import React, { useState } from 'react'
 const Calculator = props => {
     // Declare state variables
 
+    let [num, setNum] = useState(0); //defines the num
+    let [operator, setOperator] = useState(""); //operator
+    let [total, setTotal] = useState(""); //total
+    let [error, setError] = (""); //does this work?
+    let [working, setWorking] = useState(false);
+    let [num2, setNum2] = useState(0);
+
+  
+     //clear button this should be simple to do?
+     const clear = () => {
+      setTotal(0);
+      setNum("");
+      setOperator("");
+    }
+  
+    //operator function for my +, -, *, /, and i guess % <- need to look up
+    const formula = () => {
+      if (operator === '+') {
+        setTotal ( parseInt(num) + parseInt(num2))
+      } else if (operator === '-') {
+        setTotal ( parseInt(num) - parseInt(num2))
+      } else if (operator === '/') {
+        setTotal ( parseInt(num) / parseInt(num2))
+      } else if (operator === '*') {
+        setTotal ( parseInt(num) * parseInt(num2))
+      } else if (operator === '%') {
+        // 100 % 7 = 2 
+        // // a = 100, n = 7
+        // 100 - (7 * floor(100/7)) = 2 | (total / num) = totalOne | (num * totalOne) = totalTwo | total - totalTwo = whatsLeft
+        let total1 = total / parseInt(num);
+        let total2 = num * Math.floor(total1);
+        let whatsLeft = total - total2;
+        setTotal(Math.round(whatsLeft));
+      }
+    }
+  
+    let handleButtons = (e) => {
+      if (operator == '') {
+        setWorking(false)
+        setNum(num + e.target.value)
+        // formula()
+      } else {
+        setNum2(num2 + e.target.value)
+      }
+    }
+  
+    let handleOperator = (e) => {
+        setWorking(true)
+        setOperator(e.target.value)
+    } 
+
 
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p> {props.num} {props.operator} </p> 
-                <div className="answer-box">{props.total}</div>
+                <p> {num} {operator} {num2} </p> 
+                <div className="answer-box">{total}</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top"
-                        onClick={() => props.clear()}>AC</button>
-                    <button className="calc-button calc-button-top" onClick={props.handleOperator} value='+/-'>+/-</button>
-                    <button className="calc-button calc-button-top" onClick={props.handleOperator} value='%'>%</button>
-                    <button className="calc-button calc-button-op" onClick={props.handleOperator} value='/'>/</button>
+                        onClick={clear}>AC</button>
+                    <button className="calc-button calc-button-top" onClick={handleOperator} value='+/-'>+/-</button>
+                    <button className="calc-button calc-button-top" onClick={handleOperator} value='%'>%</button>
+                    <button className="calc-button calc-button-op" onClick={handleOperator} value='/'>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={props.handleButtons} value='7'>7</button>
-                    <button className="calc-button" onClick={props.handleButtons} value='8'>8</button>
-                    <button className="calc-button" onClick={props.handleButtons} value='9'>9</button>
-                    <button className="calc-button calc-button-op" onClick={props.handleOperator} value='*'>x</button>
+                    <button className="calc-button" onClick={handleButtons} value='7'>7</button>
+                    <button className="calc-button" onClick={handleButtons} value='8'>8</button>
+                    <button className="calc-button" onClick={handleButtons} value='9'>9</button>
+                    <button className="calc-button calc-button-op" onClick={handleOperator} value='*'>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={props.handleButtons} value='4'>4</button>
-                    <button className="calc-button" onClick={props.handleButtons} value='5'>5</button>
-                    <button className="calc-button" onClick={props.handleButtons} value='6'>6</button>
-                    <button className="calc-button calc-button-op" onClick={props.handleOperator} value='-'>-</button>
+                    <button className="calc-button" onClick={handleButtons} value='4'>4</button>
+                    <button className="calc-button" onClick={handleButtons} value='5'>5</button>
+                    <button className="calc-button" onClick={handleButtons} value='6'>6</button>
+                    <button className="calc-button calc-button-op" onClick={handleOperator} value='-'>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={props.handleButtons} value='1'>1</button>
-                    <button className="calc-button" onClick={props.handleButtons} value='2'>2</button>
-                    <button className="calc-button" onClick={props.handleButtons} value='3'>3</button>
-                    <button className="calc-button calc-button-op" onClick={props.handleOperator} value='+'>+</button>
+                    <button className="calc-button" onClick={handleButtons} value='1'>1</button>
+                    <button className="calc-button" onClick={handleButtons} value='2'>2</button>
+                    <button className="calc-button" onClick={handleButtons} value='3'>3</button>
+                    <button className="calc-button calc-button-op" onClick={handleOperator} value='+'>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2" onClick={props.handleButtons}>0</button>
+                    <button className="calc-button width-2" onClick={handleButtons}>0</button>
                     <button className="calc-button">.</button>
                     <button className="calc-button calc-button-op" 
-                        onClick={() => props.formula()}>=</button>
+                        onClick={formula}>=</button>
                 </div>
-                    <p>{props.error}</p>
+                    <p>{error}</p>
             </div>
         </div>
     )
