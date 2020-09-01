159ced78d0ff4ac019c09901acf610b5a8d76ab4
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-01 01:17:58 -0700

    [adv-cal]redo, but doesnt click

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,42 +1,117 @@
 import React, { useState } from 'react'
+import commafy from './Commafy'
 
 
 
 const Calculator = props => { 
     // Declare state variables
-    let [num1, setNum1] = useState('')
-    let [num2, setNum2] = useState('')
-    let [results, setResults] = useState('0')
-    let [operator,setOperator] = useState('')
+    let [value, setValue] = useState('0')
+    let [results, setResults] = useState(null)
+    let [operator,setOperator] = useState(null)
 
+    const handlePress = content => () => {
+       const num = parseFloat(value)
 
+       if (content ==="AC"){
+           setValue('0')
+           setResults(null)
+           setOperator(null)
+           return;
+       }
 
-    const handlePress = (e) => {
-        if(e.target.value !== '0' || e.target.value !==''){
-            setNum1(num1 + e.target.value)
+       if (content === '+') {
+           if(operator !== null){
+               if (operator === '+'){
+                    setResults(results + parseFloat(value))
+                } else if (operator === '-'){
+                    setResults(results - parseFloat(value))
+                } else if (operator === 'x'){
+                    setResults(results - parseFloat(value))
+                } else if (operator === '/'){
+                    setResults (results- parseFloat(value))
+                } 
+            } else {
+                setResults(parseFloat(value))
+            }
+                setValue('0')
+                setOperator('+')
+                return
+            }
+        if (content === '-') {
+            if(operator !== null){
+                if (operator === '+'){
+                    setResults(results + parseFloat(value))
+                } else if (operator === '-'){
+                    setResults(results - parseFloat(value))
+                } else if (operator === 'x'){
+                    setResults(results - parseFloat(value))
+                } else if (operator === '/'){
+                    setResults (results- parseFloat(value))
+                } 
+            } else {
+                setResults(parseFloat(value))
+            }
+                setValue('0')
+                setOperator('-')
+                return
+                }
+        if (content === '*') {
+            if(operator !== null){
+                if (operator === '+'){
+                        setResults(results + parseFloat(value))
+                    } else if (operator === '-'){
+                        setResults(results - parseFloat(value))
+                    } else if (operator === 'x'){
+                        setResults(results - parseFloat(value))
+                    } else if (operator === '/'){
+                        setResults (results- parseFloat(value))
+                    } 
+            } else {
+                setResults(parseFloat(value))
+            }
+                setValue('0')
+                setOperator('*')
+                return
+                }  
+        if (content === '/') {
+            if(operator !== null){
+                if (operator === '+'){
+                    setResults(results + parseFloat(value))
+                } else if (operator === '-'){
+                    setResults(results - parseFloat(value))
+                } else if (operator === 'x'){
+                    setResults(results - parseFloat(value))
+                } else if (operator === '/'){
+                    setResults (results- parseFloat(value))
+                } 
+            } else {
+                setResults(parseFloat(value))
+            }
+                setValue('0')
+                setOperator('/')
+                return
+                }   
+        if (content === '='){
+            if (!operator)
+            return
+            if(operator === '+'){
+                setValue((results + parseFloat(value)).toString())
+            } else if(operator === '-'){
+                setValue((results - parseFloat(value)).toString())
+            } else if(operator === 'x'){
+                setValue((results * parseFloat(value)).toString())
+            }else if(operator === '/'){
+                setValue((results / parseFloat(value)).toString())
+            }
+            setResults(null)
+            setOperator(null)
+            return
         }
-    }
-
-    const operationPress = (e) => {
-        setOperator(e.target.value)
-        setNum2(num1)
-        setNum1('')
-    }
-
-    const calculate = () => {
-        let result = ''
-        if(operator === '+') {
-            result = (num1)+(num2)
-        } 
-        if (operator === '-') {
-            result = (num1)-(num2)
-        } 
-        if (operator === '*') {
-            result = (num1)*(num2)
-        } 
-        if (operator === '/') {
-            result = (num1)/(num2)
-        } 
+        if(value[value.length - 1]==='.') {
+            setValue (value + content)
+        } else {
+            setValue(parseFloat(num + content).toString())
+        }    
     }
 
     return (
@@ -44,35 +119,35 @@ const Calculator = props => {
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Value: </p>
-                <div className="answer-box">{num1}</div>
+                <div className="answer-box">{commafy(value)}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top"  >AC</button>
-                    <button className="calc-button calc-button-top"  >+/-</button>
-                    <button className="calc-button calc-button-top"  >%</button>
-                    <button className="calc-button calc-button-op"  >/</button>
+                    <button className="calc-button calc-button-top" onClick={handlePress} type="function" content='AC'>AC</button>
+                    <button className="calc-button calc-button-top" onClick={handlePress} type="function" content='+/-'>+/-</button>
+                    <button className="calc-button calc-button-top" onClick={handlePress} type="function" content='%'>%</button>
+                    <button className="calc-button calc-button-op" onClick={handlePress} type="operator" content='/'>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={handlePress} value='7'>7</button>
-                    <button className="calc-button" onClick={handlePress} value='8'>8</button>
-                    <button className="calc-button" onClick={handlePress} value='9'>9</button>
-                    <button className="calc-button calc-button-op"  >x</button>
+                    <button className="calc-button" onClick={handlePress}  content='7'>7</button>
+                    <button className="calc-button" onClick={handlePress}  content='8'>8</button>
+                    <button className="calc-button" onClick={handlePress}  content='9'>9</button>
+                    <button className="calc-button calc-button-op" onClick={handlePress} type="operator" content='*'>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={handlePress} value='4'>4</button>
-                    <button className="calc-button" onClick={handlePress} value='5'>5</button>
-                    <button className="calc-button" onClick={handlePress} value='6'>6</button>
-                    <button className="calc-button calc-button-op"  >-</button>
+                    <button className="calc-button" onClick={handlePress}  content='4'>4</button>
+                    <button className="calc-button" onClick={handlePress}  content='5'>5</button>
+                    <button className="calc-button" onClick={handlePress}  content='6'>6</button>
+                    <button className="calc-button calc-button-op" onClick={handlePress} type="operator" content='-'>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={handlePress} value='1'>1</button>
-                    <button className="calc-button" onClick={handlePress} value='2'>2</button>
-                    <button className="calc-button" onClick={handlePress} value='3'>3</button>
-                    <button className="calc-button calc-button-op"  >+</button>
+                    <button className="calc-button" onClick={handlePress}  content='1'>1</button>
+                    <button className="calc-button" onClick={handlePress}  content='2'>2</button>
+                    <button className="calc-button" onClick={handlePress}  content='3'>3</button>
+                    <button className="calc-button calc-button-op" onClick={handlePress} type="operator" content='+'>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2" onClick={handlePress} value='0'>0</button>
-                    <button className="calc-button" >.</button>
-                    <button className="calc-button calc-button-op"  >=</button>
+                    <button className="calc-button width-2" onClick={handlePress}  content='0'>0</button>
+                    <button className="calc-button" onClick={handlePress} content='.'>.</button>
+                    <button className="calc-button calc-button-op" onClick={handlePress} type="operator" content='='>=</button>
                 </div>
             </div>
         </div>
