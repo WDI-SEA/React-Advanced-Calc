f70a5d29db63173d4249d9bb727218028bda6f43
alpha-martinez <martinezalphaa@gmail.com>
2020-08-31 22:58:56 -0400

    got my console log showing when you click on a button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import Calculator from './Calculator'
 
 function App() {
+  //useState -> the first one is its original form but the second one is what changes come next
+
   let [num, setNum] = useState(0); //defines the num
   let [operator, setOperator] = useState(""); //operator
   let [total, setTotal] = useState(""); //total
+  let [error, setError] = (""); //does this work?
 
-  //operator function
+   //clear button this should be simple to do?
+   const clear = () => {
+    setTotal(0);
+    setNum("");
+    setOperator("");
+  }
+
+  //operator function for my +, -, *, /, and i guess % <- need to look up
   const formula = () => {
     if (operator === '+') {
       setTotal(total + parseInt(num))
@@ -22,17 +32,34 @@ function App() {
       // // a = 100, n = 7
       // 100 - (7 * floor(100/7)) = 2 | (total / num) = totalOne | (num * totalOne) = totalTwo | total - totalTwo = whatsLeft
       let total1 = total / parseInt(num);
-      let total2 = num * floor(total1);
+      let total2 = num * Math.floor(total1);
       let whatsLeft = total - total2;
       setTotal(Math.round(whatsLeft));
     }
   }
 
-  //clear button
-  const clear = () => {
-    setTotal(0);
-    setNum("");
-    setOperator("");
+  // //error message & current number (how do i do this) does it need to show in my other functions?
+  // const err = () => {
+  //   if(!num){ //this is for my number
+  //     setError("Error: two operators can't be used")
+  //   } else {
+  //     //am i missing something else?
+  //   }
+  // }
+
+  //current number or current state
+  // currentNum = () => {
+  //   if(num) {
+
+  //   } else {
+
+  //   }
+  // }
+
+  let handleButtons = (e) => {
+    e.preventDefault();
+    console.log('please work')
+
   }
 
   return (
@@ -42,6 +69,10 @@ function App() {
       total={total}
       clear={clear}
       operator={operator}
+      //err={err}
+      error={error}
+      //currentNum={currentNum}
+      handleButtons={handleButtons}
       />
     </div>
   );
--- src/Calculator.js
+++ src/Calculator.js
@@ -18,29 +18,30 @@ const Calculator = props => {
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
+                    <button className="calc-button" onClick={props.handleButtons}>7</button>
+                    <button className="calc-button" onClick={props.handleButtons}>8</button>
+                    <button className="calc-button" onClick={props.handleButtons}>9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
+                    <button className="calc-button" onClick={props.handleButtons}>4</button>
+                    <button className="calc-button" onClick={props.handleButtons}>5</button>
+                    <button className="calc-button" onClick={props.handleButtons}>6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
+                    <button className="calc-button" onClick={props.handleButtons}>1</button>
+                    <button className="calc-button" onClick={props.handleButtons}>2</button>
+                    <button className="calc-button" onClick={props.handleButtons}>3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button className="calc-button width-2" onClick={props.handleButtons}>0</button>
                     <button className="calc-button">.</button>
                     <button className="calc-button calc-button-op" 
                         onClick={() => props.formula()}>=</button>
                 </div>
+                    <p>{props.error}</p>
             </div>
         </div>
     )
