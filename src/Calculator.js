865d829b79fc0a768264f54ac873e6a8043cf510
cmiltiadou <cmiltiad@gmail.com>
2021-12-01 22:08:43 -0500

    90 percent there, need to fix bug that requires two button clicks to display number

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,42 +2,119 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
+    state = {
+        operator: null,
+        num1: "",
+        num2: "",
+        display: null
+        
+    }
+
+changeOperator = (symbol) => {
+    if(this.state.num1.length > 1 && this.state.num2 === ""){
+    this.setState({
+        operator: symbol,
+        display: this.state.operator
+    })}
+    return this.state.operator
+}
+
+setNum = (value) => {
+    // console.log(value)
+    if(this.state.operator === null ){
+        this.setState({
+            num1: " " +  this.state.num1 + value,
+            display: this.state.num1
+        })
+    }else {
+        this.setState({
+            num2: " " +  this.state.num2 + value,
+            display: this.state.num2
+        })
+    }
+    return this.state.num1
+}
+
+evaluate = () => {
+
+    let newNum1 = Number(this.state.num1)
+    let newNum2 = Number(this.state.num2)
+    let answer = ""
+
+    if(this.state.operator === '/'){
+        answer = newNum1 / newNum2
+        this.setState({
+            display: answer
+        })
+    }else if(this.state.operator === '*'){
+        answer = newNum1 * newNum2
+        this.setState({
+            display: answer
+        })
+    }else if(this.state.operator === '+'){
+        answer = newNum1 + newNum2
+        this.setState({
+            display: answer
+        })
+    }else if(this.state.operator === '-'){
+        answer = newNum1 - newNum2
+        this.setState({
+            display: answer
+        })
+    }else{
+        this.setState({
+            display: NaN
+        })
+    }
+
+}
+
+allClear = () => {
+    this.setState({
+        operator: null,
+        num1: "",
+        num2: "",
+        display: ""
+    })
+
+}
 
 render(){
+
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{this.state.display}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={() => this.allClear()}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={() => this.changeOperator('/')}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={() => this.setNum(7)}>7</button>
+                    <button className="calc-button" onClick={() => this.setNum(8)}>8</button>
+                    <button className="calc-button" onClick={() => this.setNum(9)}>9</button>
+                    <button className="calc-button calc-button-op" onClick={() => this.changeOperator('*')}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={() => this.setNum(4)}>4</button>
+                    <button className="calc-button" onClick={() => this.setNum(5)}>5</button>
+                    <button className="calc-button" onClick={() => this.setNum(6)}>6</button>
+                    <button className="calc-button calc-button-op" onClick={() => this.changeOperator('-')}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={() => this.setNum(1)}>1</button>
+                    <button className="calc-button" onClick={() => this.setNum(2)}>2</button>
+                    <button className="calc-button" onClick={() => this.setNum(3)}>3</button>
+                    <button className="calc-button calc-button-op" onClick={() => this.changeOperator('+')}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button className="calc-button width-2"onClick={() => this.setNum(0)}>0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button calc-button-op" onClick={() => this.evaluate()}>=</button>
                 </div>
             </div>
         </div>
