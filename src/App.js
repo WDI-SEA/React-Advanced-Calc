70743cfa66b6afe16e77ce2cc7e05f79347047dc
anaismveras <verasmanais@gmail.com>
2021-12-01 23:21:18 -0500

    can put numbers in the answerbox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import './App.css';
-import Calculator from './Calculator'
+// import React, {Component} from 'react';
+// import './App.css';
+// import Calculator from './Calculator'
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    )
-  }
-}
+// class App extends Component {
+//   render(){
+//     return (
+//       <div className="App">
+//         <Calculator />
+//       </div>
+//     )
+//   }
+// }
 
-export default App;
+// export default App;
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,6 +2,49 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
+    state = {
+        firstNumbers: '',
+        secondNumbers: '',
+        operator: '',
+        sum: ''
+    }
+
+    handleNumber = (clicked) => {
+        const calcFunctions = ['/', '+', '*', '-']
+        let buttonValue = clicked.target.value
+        //for checking
+        // console.log('this is click button', clicked)
+        // console.log('this is buttonV', buttonValue)
+         if (!this.state.operator) {
+            this.setState({
+                firstNumbers: this.state.firstNumbers += buttonValue 
+            })
+        } else {
+            this.setState({
+                secondNumbers: this.state.secondNumbers += buttonValue
+            })
+        }
+
+        if(calcFunctions[0] == clicked) {
+            this.setState({
+                
+            })
+        }
+    }
+    
+    equalButton = () => {
+        this.setState ({
+            sum: eval(this.state.firstNumbers + this.state.operator + this.state.secondNumbers)
+        })
+    }
+
+
+    clear = () => {
+        this.setState({
+            numbersToCalc: [],
+            num: []
+        })
+    }
 
 render(){
     return (
@@ -9,35 +52,35 @@ render(){
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{this.state.firstNumbers}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button onClick={this.clear} className="calc-button calc-button-top">AC</button>
+                    <button onClick={this.handleNumber} className="calc-button calc-button-top">+/-</button>
+                    <button onClick={this.handleNumber} className="calc-button calc-button-top">%</button>
+                    <button onClick={this.handleNumber} className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button value='7' onClick={this.handleNumber} className="calc-button">7</button>
+                    <button value='8' onClick={this.handleNumber}className="calc-button">8</button>
+                    <button value='9'  onClick={this.handleNumber} className="calc-button">9</button>
+                    <button onClick={this.handleNumber} className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button value='4' onClick={this.handleNumber} className="calc-button">4</button>
+                    <button value='5' onClick={this.handleNumber} className="calc-button">5</button>
+                    <button value='6' onClick={this.handleNumber} className="calc-button">6</button>
+                    <button onClick={this.handleNumber} className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button value='1' onClick={this.handleNumber} className="calc-button">1</button>
+                    <button value='2' onClick={this.handleNumber} className="calc-button">2</button>
+                    <button value='3' onClick={this.handleNumber} className="calc-button">3</button>
+                    <button name='plus' className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button value='0' onClick={this.handleNumber} className="calc-button width-2">0</button>
+                    <button onClick={this.handleNumber} className="calc-button">.</button>
+                    <button onClick={this.equalButton} name='equal' className="calc-button calc-button-op">=</button>
                 </div>
             </div>
         </div>
