a120eaec3975418454a1eb616bfd93063df3461f
LindZCoding <Lindzgaming@gmail.com>
2021-12-01 21:18:03 -0800

    calc function works

--- src/App.js
+++ src/App.js
@@ -2,11 +2,104 @@ import React, {Component} from 'react';
 import './App.css';
 import Calculator from './Calculator'
 
+
+
 class App extends Component {
+
+  state = {
+    finalResult: '',
+    firstNumber: '',
+    secondNumber: '',
+    operator: '',
+    error: 'Try again'
+  }
+
+
+
+  calculate = () => {
+    this.setState({
+      finalResult: eval(this.state.firstNumber + this.state.operator + this.state.secondNumber)
+    })
+    
+  }
+
+  clear = () => {
+    this.setState({
+      finalResult: '',
+      firstNumber: '',
+      secondNumber: '',
+      operator: ''
+    })
+  }
+
+
+
+  onClick = (buttonClicked) => {
+    const operators = ['/', '*', '+', '-']
+
+    if (buttonClicked === '%') {
+      return;
+    }
+
+    if (buttonClicked === 'AC') {
+      this.clear()
+      return;
+    }
+
+    if (buttonClicked === '=') {
+      this.calculate()
+      this.setState({
+        firstNumber: '',
+        secondNumber: '',
+        operator: ''
+      })
+      return;
+    }
+
+    if (operators.includes(buttonClicked)) {
+      this.setState({
+        operator: buttonClicked
+      })
+      return;
+    }
+
+    if (buttonClicked === '0' && this.state.operator === '' && this.state.firstNumber === '') {
+     this.setState({
+       firstNumber: this.state.error
+     })
+     return;
+    }
+
+    if (buttonClicked == '0' && this.state.operator && this.state.firstNumber && this.state.secondNumber == '') {
+     this.setState({
+       secondNumber: this.state.error,
+       firstNumber: '',
+       operator: ''
+     })
+     return;
+    }
+    
+
+    if (!this.state.operator) {
+      this.setState({
+        firstNumber: this.state.firstNumber += buttonClicked
+      })
+    } else {
+      this.setState({
+        secondNumber: this.state.secondNumber += buttonClicked
+      })
+      return;
+    }
+    // console.log('button bein clicked right here', buttonClicked)
+    // console.log('final result', this.state.finalResult)
+    
+  }
+
+
   render(){
     return (
       <div className="App">
-        <Calculator />
+        <Calculator onClick={ this.onClick } labelDisplay={this.state.finalResult ? this.state.finalResult : `${this.state.firstNumber} ${this.state.operator} ${this.state.secondNumber}`}/>
       </div>
     )
   }
--- src/Calculator.js
+++ src/Calculator.js
@@ -9,35 +9,35 @@ render(){
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{this.props.labelDisplay}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" value='AC' onClick={e => this.props.onClick(e.target.value)}>AC</button>
+                    <button className="calc-button calc-button-top" onClick={e => this.props.onClick(e.target.value)}>+/-</button>
+                    <button className="calc-button calc-button-top" value='%' onClick={e => this.props.onClick(e.target.value)}>%</button>
+                    <button className="calc-button calc-button-op" value='/' onClick={e => this.props.onClick(e.target.value)}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" value='7' onClick={e => this.props.onClick(e.target.value)}>7</button>
+                    <button className="calc-button" value='8' onClick={e => this.props.onClick(e.target.value)}>8</button>
+                    <button className="calc-button" value='9' onClick={e => this.props.onClick(e.target.value)}>9</button>
+                    <button className="calc-button calc-button-op" value='*' onClick={e => this.props.onClick(e.target.value)}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" value='4' onClick={e => this.props.onClick(e.target.value)}>4</button>
+                    <button className="calc-button" value='5' onClick={e => this.props.onClick(e.target.value)}>5</button>
+                    <button className="calc-button" value='6' onClick={e => this.props.onClick(e.target.value)}>6</button>
+                    <button className="calc-button calc-button-op" value='-' onClick={e => this.props.onClick(e.target.value)}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" value='1' onClick={e => this.props.onClick(e.target.value)}>1</button>
+                    <button className="calc-button" value='2' onClick={e => this.props.onClick(e.target.value)}>2</button>
+                    <button className="calc-button" value='3' onClick={e => this.props.onClick(e.target.value)}>3</button>
+                    <button className="calc-button calc-button-op" value='+' onClick={e => this.props.onClick(e.target.value)}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" value='0' onClick={e => this.props.onClick(e.target.value)}>0</button>
+                    <button className="calc-button" value='.' onClick={e => this.props.onClick(e.target.value)}>.</button>
+                    <button className="calc-button calc-button-op" value='=' onClick={e => this.props.onClick(e.target.value)}>=</button>
                 </div>
             </div>
         </div>
