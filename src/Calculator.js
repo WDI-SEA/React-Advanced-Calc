e3dcc007021b134cdc085ccd8ea63fa26ee9ccec
paulinal3 <paulinal3@outlook.com>
2021-12-01 19:12:05 -0500

    pseudocoding helper methods needed and attaching to operator and number buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,46 +8,51 @@ class Calculator extends Component {
         numValue: []
     }
 
-render(){
-    return (
-        <div className="container">
-            <h1>React Calculator</h1>
-            <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">{this.state.currentNum}</div>
-                <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+    // create a function to set/pass operator once one has been clicked
+
+    // clear function that will reset states
+    // attach helper method to AC button
+
+    render() {
+        return (
+            <div className="container">
+                <h1>React Calculator</h1>
+                <div className="calc-container">
+                    <p>Values: </p>
+                    <div className="answer-box">{this.state.currentNum}</div>
+                    <div className="calc-row">
+                        <button className="calc-button calc-button-top">AC</button>
+                        <button className="calc-button calc-button-top">+/-</button>
+                        <button className="calc-button calc-button-top">%</button>
+                        <button className="calc-button calc-button-op" onClick={(e) => this.setOperator('/')}>/</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('7')}>7</button>
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('8')}>8</button>
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('9')}>9</button>
+                        <button className="calc-button calc-button-op" onClick={(e) => this.setOperator('*')}>x</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('4')}>4</button>
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('5')}>5</button>
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('6')}>6</button>
+                        <button className="calc-button calc-button-op" onClick={(e) => this.setOperator('-')}>-</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('1')}>1</button>
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('2')}>2</button>
+                        <button className="calc-button" onClick={(e) => this.inputNumVal('3')}>3</button>
+                        <button className="calc-button calc-button-op" onClick={(e) => this.setOperator('+')}>+</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button width-2" onClick={(e) => this.inputNumVal('0')}>0</button>
+                        <button className="calc-button">.</button>
+                        <button className="calc-button calc-button-op">=</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
 
 export default Calculator
