e1d12ad4ed65bbb9a0dfa4ad8713a6250fc63a46
isaac8069 <isaac8069@gmail.com>
2021-12-01 20:10:02 -0500

    function to handle numbers added

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,65 +8,88 @@ class Calculator extends Component {
         operator: ''
     }
 
-//     this.inputNumber = this.inputNumber.bind(this);
-//     this.operator = this.operator.bind(this);
-//     this.equals = this.equals.bind(this);
-//     this.handleCommaInput = this.handleCommaInput.bind(this);
-//     this.plusMinus = this.plusMinus.bind(this);
-//     this.reset = this.reset.bind(this);
-//   }
+    //     this.inputNumber = this.inputNumber.bind(this);
+    //     this.operator = this.operator.bind(this);
+    //     this.equals = this.equals.bind(this);
+    //     this.handleCommaInput = this.handleCommaInput.bind(this);
+    //     this.plusMinus = this.plusMinus.bind(this);
+    //     this.reset = this.reset.bind(this);
+    //   }
 
-  // Function to handle operator inputs
-  operator(e) {
-    const operation = e.target.innerHTML;
+    // Function to handle inputs from number buttons
+    inputNumber(e) {
+        e.preventDefault();
 
-    this.setState((prevState) => {
-      return {
-        operator: operation,
-        result: prevState.number ? prevState.number : prevState.result,
-        number: operation === '-' && prevState.number === '0' ? '-' : '',
-      };
-    });
-  }
+        // Getting the value from the pressed button
+        const value = e.target.innerHTML;
 
-    // Function to handle clear button
+        console.log(value);
+
+        // appending the value from the button to the current number
+        this.setState((prevState) => {
+            return {
+                number:
+                    prevState.number === '0' && value === '0' // prevents number from having multiple 0's at the beginning
+                        ? '0'
+                        : prevState.number % 1 === 0 && value !== '0' // if it's a whole number
+                            ? Number(prevState.number + value) // it returns a Number() to get rid of 0's in the front when a number is typed, also includes
+                            : prevState.number + value, // value !== "0" so you can type a 0 right after the comma
+                result: prevState.operator ? prevState.result : '0', // this line resets the result if a new number was typed with no operator stored
+            };
+        });
+    }
+
+    // Function to handle operator
+    operator(e) {
+        const operation = e.target.innerHTML;
+
+        this.setState((prevState) => {
+            return {
+                operator: operation,
+                result: prevState.number ? prevState.number : prevState.result,
+                number: operation === '-' && prevState.number === '0' ? '-' : '',
+            };
+        });
+    }
+
+    // Function to handle reset/clear button
     reset() {
         this.setState({ result: '0', number: '0', operator: '' });
     }
 
-      // Function to handle equal input
-  equals() {
-    let newResult = 0;
-    switch (this.state.operator) {
-      case '+':
-        newResult = Number(this.state.result) + Number(this.state.number);
-        break;
-      case '-':
-        newResult = this.state.result - this.state.number;
-        break;
-      case '*':
-        newResult = this.state.result * this.state.number;
-        break;
-      case '/':
-        newResult = this.state.result / this.state.number;
-        break;
-      default:
-        newResult = this.state.number ? this.state.number : this.state.result;
+    // Function to handle equal input
+    equals() {
+        let newResult = 0;
+        switch (this.state.operator) {
+            case '+':
+                newResult = Number(this.state.result) + Number(this.state.number);
+                break;
+            case '-':
+                newResult = this.state.result - this.state.number;
+                break;
+            case '*':
+                newResult = this.state.result * this.state.number;
+                break;
+            case '/':
+                newResult = this.state.result / this.state.number;
+                break;
+            default:
+                newResult = this.state.number ? this.state.number : this.state.result;
+        }
+
+        this.setState({ number: '', operator: '', result: Number(newResult) });
     }
 
-    this.setState({ number: '', operator: '', result: Number(newResult) });
-  }
-
- // Function to handle negation of number
- plusMinus() {
-    if (!this.state.number && this.state.result) {
-      // special case: number is empty and result isn't (e.g. after equal operation) it inverts the result
-      this.setState({ result: -this.state.result });
-    } else {
-      // default case: negates current number
-      this.setState({ number: -this.state.number });
+    // Function to handle negation of number
+    plusMinus() {
+        if (!this.state.number && this.state.result) {
+            // special case: number is empty and result isn't (e.g. after equal operation) it inverts the result
+            this.setState({ result: -this.state.result });
+        } else {
+            // default case: negates current number
+            this.setState({ number: -this.state.number });
+        }
     }
-  }
 
     render() {
         const { number, result, operator } = this.state;
