687b03f7f1cffeaa513e5f8e945c03decbd31456
anaismveras <verasmanais@gmail.com>
2021-12-02 00:17:57 -0500

    can get error message to appear

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,7 +6,8 @@ class Calculator extends Component {
         firstNumbers: '',
         secondNumbers: '',
         operator: '',
-        sum: ''
+        sum: '',
+        error: 'Can not press 0 first'
     }
 
     handleNumber = (clicked) => {
@@ -15,34 +16,56 @@ class Calculator extends Component {
         //for checking
         // console.log('this is click button', clicked)
         // console.log('this is buttonV', buttonValue)
+        if(calcFunctions.includes(buttonValue)) {
+            this.setState({
+                operator: buttonValue
+            })
+            return
+        } 
+
          if (!this.state.operator) {
             this.setState({
-                firstNumbers: this.state.firstNumbers += buttonValue 
+                firstNumbers: this.state.firstNumbers += buttonValue
             })
+            if(this.state.firstNumbers == '0') {
+                //checking
+                // console.log('this is wrong')
+                this.setState({
+                    firstNumbers: this.state.error
+                })
+            } 
         } else {
             this.setState({
                 secondNumbers: this.state.secondNumbers += buttonValue
             })
-        }
-
-        if(calcFunctions[0] == clicked) {
-            this.setState({
-                
-            })
+            if(this.state.secondNumbers == '0') {
+                //checking
+                // console.log('this is wrong')
+                this.setState({
+                    secondNumbers: this.state.error,
+                    firstNumbers: '',
+                    operator: ''
+                })
+            } 
+            return
         }
     }
     
     equalButton = () => {
         this.setState ({
-            sum: eval(this.state.firstNumbers + this.state.operator + this.state.secondNumbers)
+            sum: eval(this.state.firstNumbers + this.state.operator + this.state.secondNumbers),
+            firstNumbers: '',
+            secondNumbers: '',
+            operator: ''
         })
     }
 
-
     clear = () => {
         this.setState({
-            numbersToCalc: [],
-            num: []
+            firstNumbers: '',
+            secondNumbers: '',
+            operator: '',
+            sum: ''
         })
     }
 
@@ -52,35 +75,35 @@ render(){
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">{this.state.firstNumbers}</div>
+                <div className="answer-box">{this.state.firstNumbers} {this.state.operator} {this.state.secondNumbers} {this.state.sum} {this.equalButton}</div>
                 <div className="calc-row">
                     <button onClick={this.clear} className="calc-button calc-button-top">AC</button>
-                    <button onClick={this.handleNumber} className="calc-button calc-button-top">+/-</button>
-                    <button onClick={this.handleNumber} className="calc-button calc-button-top">%</button>
-                    <button onClick={this.handleNumber} className="calc-button calc-button-op">/</button>
+                    <button value='+/-' onClick={this.handleNumber} className="calc-button calc-button-top">+/-</button>
+                    <button value='%'onClick={this.handleNumber} className="calc-button calc-button-top">%</button>
+                    <button value='/' onClick={this.handleNumber} className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
                     <button value='7' onClick={this.handleNumber} className="calc-button">7</button>
                     <button value='8' onClick={this.handleNumber}className="calc-button">8</button>
                     <button value='9'  onClick={this.handleNumber} className="calc-button">9</button>
-                    <button onClick={this.handleNumber} className="calc-button calc-button-op">x</button>
+                    <button value='*' onClick={this.handleNumber} className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
                     <button value='4' onClick={this.handleNumber} className="calc-button">4</button>
                     <button value='5' onClick={this.handleNumber} className="calc-button">5</button>
                     <button value='6' onClick={this.handleNumber} className="calc-button">6</button>
-                    <button onClick={this.handleNumber} className="calc-button calc-button-op">-</button>
+                    <button value='-' onClick={this.handleNumber} className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
                     <button value='1' onClick={this.handleNumber} className="calc-button">1</button>
                     <button value='2' onClick={this.handleNumber} className="calc-button">2</button>
                     <button value='3' onClick={this.handleNumber} className="calc-button">3</button>
-                    <button name='plus' className="calc-button calc-button-op">+</button>
+                    <button value='+' onClick={this.handleNumber} className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
                     <button value='0' onClick={this.handleNumber} className="calc-button width-2">0</button>
-                    <button onClick={this.handleNumber} className="calc-button">.</button>
-                    <button onClick={this.equalButton} name='equal' className="calc-button calc-button-op">=</button>
+                    <button value='.' onClick={this.handleNumber} className="calc-button">.</button>
+                    <button value='=' onClick={this.equalButton} className="calc-button calc-button-op">=</button>
                 </div>
             </div>
         </div>
