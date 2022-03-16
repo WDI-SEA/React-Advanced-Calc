93d12ee7408fb48040a9eff48232a9aba9e58c49
danieldenton <danielmdenton@gmail.com>
2022-03-15 22:08:29 -0700

    undone

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,48 +1,120 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 
-class Calculator extends Component {
-    // Declare state variables
+export default function Calculator() {
+  let [num, setNum] = useState("");
+  let [prevNum, setPrevNum] = useState("");
+  let [operator, setOperator] = useState("");
+  let [result, setResult] = useState("0");
 
-render(){
-    return (
-        <div className="container">
-            <h1>React Calculator</h1>
-            <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
-                <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
-                </div>
-            </div>
+  const changeState = (e) => {
+    setNum(num + e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1>React Calculator</h1>
+      <div className="calc-container">
+        <p>Values: </p>
+        <div className="answer-box">{num}</div>
+        <div className="calc-row">
+          <button className="calc-button calc-button-top">AC</button>
+          <button className="calc-button calc-button-top">+/-</button>
+          <button className="calc-button calc-button-top">%</button>
+          <button className="calc-button calc-button-op">/</button>
         </div>
-    )
+        <div className="calc-row">
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="7"
+          >
+            7
+          </button>
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="8"
+          >
+            8
+          </button>
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="9"
+          >
+            9
+          </button>
+          <button
+            // onClick=
+            className="calc-button calc-button-op"
+          >
+            x
+          </button>
+        </div>
+        <div className="calc-row">
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="4"
+          >
+            4
+          </button>
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="5"
+          >
+            5
+          </button>
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="6"
+          >
+            6
+          </button>
+          <button
+            // onClick={() => setOperation("-")}
+            className="calc-button calc-button-op"
+          >
+            -
+          </button>
+        </div>
+        <div className="calc-row">
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="1"
+          >
+            1
+          </button>
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="2"
+          >
+            2
+          </button>
+          <button
+            onClick={(e) => changeState(e.target.innerText)}
+            className="calc-button"
+            value="3"
+          >
+            3
+          </button>
+          <button
+            // onClick={() => setOperation("+")}
+            className="calc-button calc-button-op"
+          >
+            +
+          </button>
+        </div>
+        <div className="calc-row">
+          <button className="calc-button width-2">0</button>
+          <button className="calc-button">.</button>
+          <button className="calc-button calc-button-op">=</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-}
-
-export default Calculator
