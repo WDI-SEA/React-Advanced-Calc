8c61359cb6ecbbf60c9835201c6a4dd6022be995
Professorsurf <guevara.a.gabriel@gmail.com>
2022-03-15 22:24:01 -0700

    3-15 deliverable

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,9 +1,91 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Calculator extends Component {
+const Calculator = props => {
+    let [answer, setAnswer] = useState('0')
+    let [currentNum, setCurrentNum] = useState ('')
+    let [error, setError] = useState ('')
+    let [num1, setNum1] = useState ('')
+    let [operator, setOperator] = useState ('')
+
+    const numPress = e => {
+        console.log(e.target.value, 'was pressed')
+        if (e.target.value !== '0' || currentNum !== '') {
+            setCurrentNum(currentNum + e.target.value)
+        }
+    }
+
+    const dotPress = () => {
+        if (!currentNum) {
+            setCurrentNum('0.')
+        }
+        else if (currentNum.indexOf('.') === -1){
+            setCurrentNum(currentNum + '.')
+        }
+    }
+
+    const operatorPress = e => {
+        console.log(e.target.value, 'was pressed')
+            if (operator) {
+                setError('Operator has already been set. Press clear to reset.')
+            }
+            else if (!currentNum) {
+                setError('A number must be pressed first')
+            }
+            else {
+            setNum1(currentNum)
+            setOperator(e.target.value)
+            setError('')
+            setCurrentNum('')
+            }
+    }
+    const plusMinus = () => {
+        if (currentNum) {
+            setCurrentNum(currentNum * -1)
+        }
+    }
+// eslint-disable-next-line
+    const clear = () => {
+        setAnswer('0')
+        setCurrentNum('')
+        setError('')
+        setNum1('')
+        setOperator('')
+    }
+// eslint-disable-next-line
+    const calculate = () => {
+        console.log('Attempting', num1, operator, currentNum)
+        if (!operator || !num1) {
+            setError('Please enter a valid expression')
+        }
+        else if (!currentNum) {
+            setError('Enter a second number...')
+        }
+        else {
+            let tempAnswer = ''
+            if (operator === '+') {
+                tempAnswer = Number(num1) + Number(currentNum)
+            }
+            else if (operator === '-') {
+                tempAnswer = Number(num1) - Number(currentNum)
+            }
+            else if (operator === '*') {
+                tempAnswer = Number(num1) * Number(currentNum)
+            }
+            else if (operator === '/') {
+                tempAnswer = Number(num1) / Number(currentNum)
+            }
+            else if (operator === '%') {
+                tempAnswer = Number(num1) % Number(currentNum)
+            }
+            setAnswer((tempAnswer.toString()).slice(0, 10))
+            setError('')
+            setCurrentNum((tempAnswer.toString()).slice(0, 10))
+            setOperator('')
+            setNum1('')
+
+        }
+    }
     // Declare state variables
-
-render(){
     return (
         <div className="container">
             <h1>React Calculator</h1>
@@ -43,6 +125,6 @@ render(){
         </div>
     )
 }
-}
+
 
 export default Calculator
